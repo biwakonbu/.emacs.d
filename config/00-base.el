6ca03f9001d2166873921095ac8fa3c1e856789e
Ryo HIGASHIGAWA <ryo.higasigawa@gmail.com>
2015-06-24 17:53:06 +0900

    Delete byte-compile function

--- config/00-base.el
+++ config/00-base.el
@@ -1,7 +1,2 @@
-; Emacs file auto-byte-compile
-(require 'auto-async-byte-compile)
-(setq auto-async-byte-compile-excluede-files-regexp "~/.emacs.d/temp/")
-(add-hook 'emacs-lisp-mode-hook 'enable-auto-async-byte-compile-mode)
-
-; auto revert file-buffer
+;; auto revert file-buffer
 (global-auto-revert-mode 1)
