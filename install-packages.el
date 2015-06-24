9a9f4d1754268c3b727fb624cf883aba95b16f27
Ryo HIGASHIGAWA <ryo.higasigawa@gmail.com>
2015-06-24 17:53:20 +0900

    Delete byte-compile package

--- install-packages.el
+++ install-packages.el
@@ -1,10 +1,13 @@
-;-- install packages --;
-
+;;-- install packages --;;
+;; Base Setting
 (el-get-bundle init-loader)
 (el-get-bundle helm)
-(el-get-bundle auto-async-byte-compile)
 
+;; Key setting
 (el-get-bundle ddskk)
 (el-get-bundle bind-key)
+
+;; Screen setting
+;;(el-get-bundle centerd-cursor-mode)
 
 (provide 'install-packages)
