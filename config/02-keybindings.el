78aa8588e5a62310a680f008c97e236907072c77
Ryo HIGASHIGAWA <ryo.higasigawa@gmail.com>
2015-07-09 12:40:31 +0900

    Add ac-helm key-bindings.

--- config/02-keybindings.el
+++ config/02-keybindings.el
@@ -1,3 +1,11 @@
 (require 'bind-key)
 
+;; global key settings.
 (bind-key* "C-h" 'delete-backward-char)
+
+;; auto-compete with helm settings.
+(bind-key* "C-;" 'ac-complete-with-helm)
+(bind-key "C-;" 'ac-complete-with-helm ac-complete-mode-map)
+
+;; kill helm C-h key. to backspace.
+(bind-key "C-h" nil helm-map)
