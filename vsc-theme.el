a73e8c65832411697bdcca2f4ca966da5cc8a115
Ryo HIGASHIGAWA <ryo.higasigawa@gmail.com>
2015-08-01 13:57:42 +0900

    Add web-mode style.

--- vsc-theme.el
+++ vsc-theme.el
@@ -50,10 +50,10 @@
  
  ;; モードライン
  '(mode-line ((t (:foreground "#F8F8F2" :background "#000000"
-                  :box (:line-width 1 :color "#000000" :style released-button)))))
+                              :box (:line-width 1 :color "#000000" :style released-button)))))
  '(mode-line-buffer-id ((t (:foreground nil :background nil))))
  '(mode-line-inactive ((t (:foreground "#BCBCBC" :background "#333333"
-                           :box (:line-width 1 :color "#333333")))))
+                                       :box (:line-width 1 :color "#333333")))))
 
  ;; ハイライト
  '(highlight ((t (:background "#C4BE89"))))
@@ -96,6 +96,27 @@
  ;; dired
  '(dired-directory ((t (:foreground "#A6E22A"))))
  '(dired-symlink ((t (:foreground "#66D9EF"))))
+
+ ;; web-mode. colors.
+ ;;; blue
+ '(web-mode-doctype-face
+   ((t (:foreground "#4A8ACA"))))
+ '(web-mode-html-tag-face
+   ((t (:foreground "#4A8ACA"))))
+ '(web-mode-html-attr-name-face
+   ((t (:foreground "#87CEEB"))))
+ '(web-mode-html-attr-value-face
+   ((t (:foreground "#D78181"))))
+ '(web-mode-comment-face
+   ((t (:foreground "#587F35"))))
+ '(web-mode-server-comment-face
+   ((t (:foreground "#587F35"))))
+ '(web-mode-css-rule-face
+   ((t (:foreground "#A0D8EF"))))
+ '(web-mode-css-pseudo-class-face
+   ((t (:foreground "#FF7F00"))))
+ '(web-mode-css-at-rule-face
+   ((t (:foreground "#FF7F00"))))
 
  ;; MMM-mode
  '(mmm-default-submode-face ((t (:foreground nil :background "#000000")))))
