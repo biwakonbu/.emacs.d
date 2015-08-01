8ddfb2684d8413ffe08b6b22ff87b746b184220a
Ryo HIGASHIGAWA <ryo.higasigawa@gmail.com>
2015-08-01 14:31:16 +0900

    Update web-mode style

--- vsc-theme.el
+++ vsc-theme.el
@@ -98,25 +98,33 @@
  '(dired-symlink ((t (:foreground "#66D9EF"))))
 
  ;; web-mode. colors.
- ;;; blue
  '(web-mode-doctype-face
    ((t (:foreground "#4A8ACA"))))
  '(web-mode-html-tag-face
    ((t (:foreground "#4A8ACA"))))
  '(web-mode-html-attr-name-face
    ((t (:foreground "#87CEEB"))))
+ '(web-mode-html-attr-equal-face
+   ((t (:foreground "#FFFFFF"))))
  '(web-mode-html-attr-value-face
    ((t (:foreground "#D78181"))))
  '(web-mode-comment-face
    ((t (:foreground "#587F35"))))
  '(web-mode-server-comment-face
    ((t (:foreground "#587F35"))))
- '(web-mode-css-rule-face
-   ((t (:foreground "#A0D8EF"))))
- '(web-mode-css-pseudo-class-face
-   ((t (:foreground "#FF7F00"))))
+ ;;; web-mode. css colors.
  '(web-mode-css-at-rule-face
-   ((t (:foreground "#FF7F00"))))
+   ((t (:foreground "#DFCF44"))))
+ '(web-mode-comment-face
+   ((t (:foreground "#587F35"))))
+ '(web-mode-css-selector-face
+   ((t (:foreground "#DFCF44"))))
+ '(web-mode-css-pseudo-class
+   ((t (:foreground "#DFCF44"))))
+ '(web-mode-css-property-name-face
+   ((t (:foreground "#87CEEB"))))
+ '(web-mode-css-string-face
+   ((t (:foreground "#D78181"))))
 
  ;; MMM-mode
  '(mmm-default-submode-face ((t (:foreground nil :background "#000000")))))
