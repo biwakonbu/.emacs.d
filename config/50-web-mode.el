b87d3928687b2f8e2ce8358777b7a1787c4d97cc
Ryo HIGASHIGAWA <ryo.higasigawa@gmail.com>
2015-11-17 13:35:54 +0900

    add web-mode function .html extention to mako engine

--- config/50-web-mode.el
+++ config/50-web-mode.el
@@ -5,11 +5,14 @@
 ;; インデント関係
 (defun web-mode-hook ()
   "Hooks for Web mode."
-  (setq web-mode-html-offset   2)
-  (setq web-mode-css-offset    2)
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-offset 2)
   (setq web-mode-script-offset 2)
-  (setq web-mode-php-offset    2)
-  (setq web-mode-java-offset   2)
-  (setq web-mode-asp-offset    2)
-  (setq tab-width 2))
+  (setq web-mode-php-offset 2)
+  (setq web-mode-java-offset 2)
+  (setq web-mode-asp-offset 2)
+  (setq tab-width 2)
+  (setq web-mode-engines-alist
+        '(("mako" . "\\.html\\'"))))
+
 (add-hook 'web-mode-hook 'web-mode-hook)
