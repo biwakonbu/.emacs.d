926d294bf73831036187651c591cf762ea8da8a0
Ryo HIGASHIGAWA <ryo.higasigawa@gmail.com>
2015-11-17 13:58:13 +0900

    add anzu settings

--- config/01-face.el
+++ config/01-face.el
@@ -6,3 +6,16 @@
 
 ;; カーソルの位置を真ん中あたりに固定する
 ;;(require 'smooth-scrolling)
+
+
+;; anzu
+(require 'anzu)
+(global-anzu-mode +1)
+(set-face-attribute 'anzu-mode-line nil
+                    :foreground "yellow" :weight 'light)
+
+(custom-set-variables
+ '(anzu-mode-lighter "")
+ '(anzu-deactivate-region t)
+ '(anzu-search-threshold 1000)
+ '(anzu-replace-to-string-separator " => "))
