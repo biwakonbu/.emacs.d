c057a57ef87a1cc858778add82770c402f543db7
Ryo HIGASHIGAWA <ryo.higasigawa@gmail.com>
2015-11-17 12:31:19 +0900

    flymake の設定

---
+++ config/21-python-mode.el
@@ -0,0 +1,18 @@
+;;(add-hook 'python-mode-hook 'py-autopep8-enable-on-save)
+(require 'tramp-cmds)
+(when (load "flymake" t)
+  (defun flymake-pyflakes-init ()
+     ; Make sure it's not a remote buffer or flymake would not work
+     (when (not (subsetp (list (current-buffer)) (tramp-list-remote-buffers)))
+      (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                         'flymake-create-temp-inplace))
+             (local-file (file-relative-name
+                          temp-file
+                          (file-name-directory buffer-file-name))))
+        (list "pyflakes" (list local-file)))))
+  (add-to-list 'flymake-allowed-file-name-masks
+               '("\\.py\\'" flymake-pyflakes-init)))
+ 
+(add-hook 'python-mode-hook
+          (lambda ()
+            (flymake-mode t)))
