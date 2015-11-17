03534c82b0cff01e1cdb6986ac13516d44d4fd50
Ryo HIGASHIGAWA <ryo.higasigawa@gmail.com>
2015-11-17 13:58:25 +0900

    add anzu keybindings

--- config/99-keybindings.el
+++ config/99-keybindings.el
@@ -7,6 +7,10 @@
 (bind-key* "C-;" 'ac-complete-with-helm)
 (bind-key "C-;" 'ac-complete-with-helm ac-complete-mode-map)
 
+;; anzu keys
+(bind-key* "M-%" 'anzu-query-replace)
+(bind-key* "C-M-%" 'anzu-query-replace-regexp)
+
 ;; kill helm C-h key. to backspace.
 (bind-key "C-h" nil helm-map)
 
