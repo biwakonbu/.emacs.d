1c41aa34b426fc9b4126e1af07e1ba9ca3f2adf4
Ryo HIGASHIGAWA <ryo.higasigawa@gmail.com>
2015-11-17 17:41:27 +0900

    add space and tab faces

--- config/01-face.el
+++ config/01-face.el
@@ -19,3 +19,25 @@
  '(anzu-deactivate-region t)
  '(anzu-search-threshold 1000)
  '(anzu-replace-to-string-separator " => "))
+
+;; wide space and tab string faces
+(defface bigspace/foreground '((t (:background "SeaGreen"))) nil)
+(defface tab/foreground '((t (:background "#315C6E"))) nil)
+(defface space/foreground '((t (:foreground "red" :underline t))) nil)
+(defvar bigspace/foreground 'bigspace/foreground)
+(defvar tab/foreground 'tab/foreground)
+(defvar space/foreground 'space/foreground)
+(defadvice font-lock-mode (before my-font-lock-mode ())
+  (font-lock-add-keywords
+   major-mode
+   '(
+     ("　" 0 bigspace/foreground append)
+     ("\t" 0 tab/foreground append)
+     ("[ ]+$" 0 space/foreground append)
+     )))
+(ad-enable-advice 'font-lock-mode 'before 'my-font-lock-mode)
+(ad-activate 'font-lock-mode)
+(add-hook 'find-file-hooks '(lambda ()
+                              (if font-lock-mode
+                                  nil
+                                (font-lock-mode t))))
