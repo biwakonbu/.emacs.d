7f1b7d982cb93c18ddf0d8bcfda3b1e5e2f526da
Ryo HIGASHIGAWA <ryo.higasigawa@gmail.com>
2016-03-01 13:30:29 +0900

    update key-bindings

--- lisp/config/99-keybindings.el
+++ lisp/config/99-keybindings.el
@@ -6,6 +6,14 @@
 ;; auto-compete with helm settinggs.
 (bind-key* "C-;" 'auto-complete)
 (bind-key "C-;" 'auto-complete ac-mode-map)
+;; ac-menu-map.
+(bind-key "C-p" 'ac-next ac-menu-map)
+(bind-key "C-n" 'ac-previous ac-menu-map)
+(bind-key "C-s" 'ac-isearch ac-menu-map)
+
+;; helm
+(bind-key "[TAB]" 'helm-execute-persistent-action helm-read-file-map)
+(bind-key "[TAB]" 'helm-execute-persistent-action helm-find-files-map)
 
 ;; anzu keys
 (bind-key* "M-%" 'anzu-query-replace)
