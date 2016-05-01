6b746e04a6e09accdec94bce715fa182f3ee5b16
Ryo HIGASHIGAWA <ryo.higasigawa@gmail.com>
2016-05-01 17:05:27 +0900

    fix config

--- lisp/config/10-helm.el
+++ lisp/config/10-helm.el
@@ -3,18 +3,19 @@
 (require 'helm-mode)
 
 (if (eq system-type 'darwin)
-    (defadvice helm-mode (around avoid-read-file-name activate)
-      (let ((read-file-name-function read-file-name-function)
-            (completing-read-function completing-read-function))
-        ad-do-it))
-  (setq completing-read-function 'my-helm-completing-read-default)
-  (defun my-helm-completing-read-default (&rest _)
-    (apply (cond ;; [2014-08-11 Mon]helm版のread-file-nameは重いからいらない
-            ((eq (nth 1 _) 'read-file-name-internal)
-             'completing-read-default)
-            (t
-             'helm--completing-read-default))
-           _)))
+    (progn
+      (defadvice helm-mode (around avoid-read-file-name activate)
+        (let ((read-file-name-function read-file-name-function)
+              (completing-read-function completing-read-function))
+          ad-do-it))
+      (setq completing-read-function 'my-helm-completing-read-default)
+      (defun my-helm-completing-read-default (&rest _)
+        (apply (cond ;; [2014-08-11 Mon]helm版のread-file-nameは重いからいらない
+                ((eq (nth 1 _) 'read-file-name-internal)
+                 'completing-read-default)
+                (t
+                 'helm--completing-read-default))
+               _))))
 
 (with-eval-after-load "persp-mode"
   (with-eval-after-load "helm-mode"
