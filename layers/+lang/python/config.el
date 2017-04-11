9bf6e68643a400d21204836fabad8d0918619085
Ryo HIGASHIGAWA <ryo.higasigawa@gmail.com>
2017-04-12 00:45:45 +0900

    move python settings to layers dir

---
+++ layers/+lang/python/config.el
@@ -0,0 +1,33 @@
+;; pyenv package. url: https://github.com/cyberved/pyenv.el
+(require 'pyenv)
+(setq pyenv-show-active-python-in-modeline nil)
+(setq pyenv-modeline-function 'pyenv--modeline-plain)
+(global-pyenv-mode)
+
+;;(add-hook 'python-mode-hook 'py-autopep8-enable-on-save)
+(require 'ac-python)
+(require 'tramp-cmds)
+
+;; flymake
+(when (load "flymake" t)
+  (defun flymake-pyflakes-init ()
+     ; Make sure it's not a remote buffer or flymake would not work
+     (when (not (subsetp (list (current-buffer)) (tramp-list-remote-buffers)))
+      (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                         'flymake-create-temp-inplace))
+             (local-file (file-relative-name
+                          temp-file
+                          (file-name-directory buffer-file-name))))
+        (list "pyflakes" (list local-file)))))
+  (add-to-list 'flymake-allowed-file-name-masks
+               '("\\.py\\'" flymake-pyflakes-init)))
+
+(add-hook 'python-mode-hook
+          (lambda ()
+            (flymake-mode t)))
+
+(add-hook 'python-mode-hook 'jedi:setup)
+(setq jedi:complete-on-dot t)
+
+;; pyflakes
+(setq flymake-gui-warnings-enabled nil)
