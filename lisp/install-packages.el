46d0e61e98e72afc1a65a22fa137074afba3e575
Ryo HIGASHIGAWA <ryo.higasigawa@gmail.com>
2017-04-18 22:36:30 +0900

    delete package

--- lisp/install-packages.el
+++ lisp/install-packages.el
@@ -4,9 +4,6 @@
 
 (quelpa 'use-package t)
 
-;; input support mode
-(quelpa 'ac-html t)
-
 ;; buffer search package
 (quelpa 'anzu t)
 (quelpa 'avy t)
@@ -16,14 +13,5 @@
           :repo "Bad-ptr/persp-mode.el"
           :fetcher github
           :stable t))
-
-;; python
-;;(el-get-bundle py-autopep8)
-(quelpa 'flymake t)
-(quelpa 'flymake-cursor t)
-(quelpa 'flymake-easy t)
-(quelpa 'flymake-python-pyflakes t)
-
-(quelpa 'smartparens t)
 
 (provide 'install-packages)
