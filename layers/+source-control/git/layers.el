c200b951ea6b0803eadbd88de6bd169e4974683d
Ryo HIGASHIGAWA <ryo.higasigawa@gmail.com>
2017-04-26 22:33:20 +0900

    add git layer package

--- layers/+source-control/git/layers.el
+++ layers/+source-control/git/layers.el
@@ -1,5 +1,9 @@
 (defconst git/layers/install-packages
-  '(magit))
+  '(magit
+    gitignore-mode
+    gitconfig-mode
+    gitattributes-mode
+    smeargle))
 
 (if (executable-find "git")
     (layers/install-packages git/layers/install-packages))
