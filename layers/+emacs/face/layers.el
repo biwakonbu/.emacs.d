40da225db673a197b40efe765e32a6a87483ef6b
Ryo HIGASHIGAWA <ryo.higasigawa@gmail.com>
2017-04-26 22:38:43 +0900

    add emacs-face layers package

--- layers/+emacs/face/layers.el
+++ layers/+emacs/face/layers.el
@@ -1,8 +1,17 @@
 (defconst emacs-face/layers/install-packages
-  '(spacemacs-theme
+  '(font-lock
     powerline
-    spaceline
+    (spaceline
+     :repo "TheBB/spaceline"
+     :fetcher github)
+    spaceline-config
+    spacemacs-theme
     mode-icons
-    spaceline-config))
+    (all-the-icons
+     :repo "domtronn/all-the-icons"
+     :fetcher github)
+    (spaceline-all-the-icons
+     :repo "domtronn/spaceline-all-the-icons.el"
+     :fetcher github)))
 
 (layers/install-packages emacs-face/layers/install-packages)
