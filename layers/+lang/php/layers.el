4d307b2774f1b65056fce920695a10e8e9d5ccc1
Ryo HIGASHIGAWA <ryo.higasigawa@gmail.com>
2017-04-27 12:14:16 +0900

    change .php file open web-mode

--- layers/+lang/php/layers.el
+++ layers/+lang/php/layers.el
@@ -2,8 +2,11 @@
   '(eldoc
     flycheck
     php-auto-snippets
-    (php-extras :location (recipe :fetcher github :repo "arnested/php-extras"))
-    php-mode
-    phpcbf))
+    (php-extras
+     :repo "arnested/php-extras"
+     :fetcher github)
+    web-mode
+    php-mode))
+    ;;phpcbf))
 
 (layers/install-packages php/layers/install-packages)
