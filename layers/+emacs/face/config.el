6ecc73e1dcbb86d3269a07b1ce066dd0b956992f
Ryo HIGASHIGAWA <ryo.higasigawa@gmail.com>
2017-04-29 22:55:31 +0900

    add fonts custom install

--- layers/+emacs/face/config.el
+++ layers/+emacs/face/config.el
@@ -1,20 +1,50 @@
 ;; ac-mode
 (ac-config-default)
 
-;; anti aging power-line.
-(setq ns-use-srgb-colorspace nil)
-(mode-icons-mode)
-(setq mode-icons-grayscale-transform nil)
+;; (mode-icons-mode)
+;; (use-package font-lock+ :ensure t :after all-the-icons)
+;; (use-package all-the-icons :if window-system)
 
-(require 'spaceline-config)
-(spaceline-emacs-theme)
+(defun all-the-icons-custom-install-fonts (&optional pfx)
+  "Helper function to download and install the latests fonts based on OS.
+When PFX is non-nil, ignore the prompt and just install"
+  (interactive "P")
+  (when (or pfx (yes-or-no-p "This will download and install fonts, are you sure you want to do this?"))
+    (let* ((url-format "https://github.com/domtronn/all-the-icons.el/blob/master/fonts/%s?raw=true")
+           (font-dest (cl-case window-system
+                        (x  (concat (getenv "HOME") "/.fonts/"))    ;; Default Linux install directory
+                        (ns (concat (getenv "HOME") "/Library/Fonts/" ))))  ;; Default MacOS install directory
+           (known-dest? (stringp font-dest)))
+      (unless font-dest
+        (setq font-dest (read-directory-name "Font installation directory: " "~/")))
+      (mapc (lambda (font)
+              (url-copy-file (format url-format font) (concat font-dest font) t))
+            all-the-icons-font-names)
+      (when known-dest?
+        (message "Fonts downloaded, updating font cache... <fc-cache -f -v> ")
+        (shell-command-to-string (format "fc-cache -f -v")))
+      (message "%s Successfully %s `all-the-icons' fonts to `%s'!"
+               (all-the-icons-wicon "stars" :v-adjust 0.0) (if known-dest? "installed" "downloaded") font-dest))))
+
+;; (use-package spaceline-all-the-icons :after spaceline)
+;; (use-package spaceline :after powerline :ensure t
+;;   :config (setq mode-line-format (spaceline-all-the-icons-theme))
+(use-package spaceline-config
+  :init
+  (progn
+    (setq powerline-default-separator 'slant)
+    ;; anti aging power-line.
+    (setq ns-use-srgb-colorspace nil))
+  :config
+  (progn
+    (spaceline-emacs-theme)))
+
+;; paradox mode on
+(spaceline-toggle-paradox-menu-on)
 
 ;; (require 'spaceline-config)
 ;; other-theme
 ;; (spaceline-spaceemacs-theme)
-
-(setq powerline-default-separator 'slant)
-(spaceline-compile)
 
 ;;-- interface settings --;;
 
