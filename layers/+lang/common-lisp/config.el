6ace12065f4b23700a1513f28b2afd4309d0539a
Ryo HIGASHIGAWA <ryo.higasigawa@gmail.com>
2017-04-30 23:31:03 +0900

    update check roswell

--- layers/+lang/common-lisp/config.el
+++ layers/+lang/common-lisp/config.el
@@ -2,10 +2,12 @@
 
 (slime-setup '(slime-repl slime-fancy slime-banner))
 
-(setq inferior-lisp-program "ros -Q run")
-(setq lisp-body-indent 2)
+(if (executable-find "ros")
+    (progn
+      (setq inferior-lisp-program "ros -Q run")
+      (setf slime-lisp-implementations
+            `((sbcl    ("sbcl" "--dynamic-space-size" "2000"))
+              (roswell ("ros" "-Q" "run"))))
+      (setf slime-default-lisp 'roswell)))
 
-(setf slime-lisp-implementations
-      `((sbcl    ("sbcl" "--dynamic-space-size" "2000"))
-        (roswell ("ros" "-Q" "run"))))
-(setf slime-default-lisp 'roswell)
+(setq lisp-body-indent 2)
