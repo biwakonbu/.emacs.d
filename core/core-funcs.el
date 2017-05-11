868ac0a23fb0c3656f5c87d43fb82a2c768a03d2
Ryo HIGASHIGAWA <ryo.higasigawa@gmail.com>
2017-05-12 01:30:42 +0900

    add all-file-byte-compile function

--- core/core-funcs.el
+++ core/core-funcs.el
@@ -1,2 +1,8 @@
+(defun core/all-elisp-byte-compile ()
+  (interactive)
+  (let ((elisp-paths (split-string (shell-command-to-string "find $HOME/.emacs.d/ -name '*.el'"))))
+    (dolist (elisp-path elisp-paths)
+      (byte-compile-file elisp-path)
+      (message "compiled %s file." elisp-path))))
 
 (provide 'core-funcs)
