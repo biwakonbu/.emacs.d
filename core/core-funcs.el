d3763eb3d6e2a6fd81d457fb437a5f31c3ff4a92
Ryo HIGASHIGAWA <ryo.higasigawa@gmail.com>
2017-06-07 12:42:58 +0900

    add no ask kill buffers commad

--- core/core-funcs.el
+++ core/core-funcs.el
@@ -1,8 +1,16 @@
+(require 'cl)
+
 (defun core/all-elisp-byte-compile ()
   (interactive)
   (let ((elisp-paths (split-string (shell-command-to-string "find $HOME/.emacs.d/ -name '*.el'"))))
     (dolist (elisp-path elisp-paths)
       (byte-compile-file elisp-path)
       (message "compiled %s file." elisp-path))))
+
+(defun core-kill-buffers (regexp)
+  "Kill buffers matching REGEXP without asking for confirmation."
+  (interactive "sKill buffers matching this regular expression: ")
+  (flet ((kill-buffer-ask (buffer) (kill-buffer buffer)))
+    (kill-matching-buffers regexp)))
 
 (provide 'core-funcs)
