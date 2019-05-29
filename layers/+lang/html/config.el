09ee1c4ee4ec2d43179c03be47293db3149f87d8
Higashigawa Ryo <higashikawap@unzip.co.jp>
2019-05-29 15:32:07 +0900

    update indentation php & web-mode

--- layers/+lang/html/config.el
+++ layers/+lang/html/config.el
@@ -3,21 +3,23 @@
 (add-to-list 'auto-mode-alist '("\\.html" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.volt" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.jinja" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.blade\\." . web-mode))
 
 ;; インデント関係
 (defun web-mode-hook ()
   "Hooks for Web mode."
-  (setq web-mode-markup-indent-offset 2)
-  (setq web-mode-css-offset 2)
-  (setq web-mode-script-offset 2)
-  (setq web-mode-php-offset 2)
-  (setq web-mode-java-offset 2)
-  (setq web-mode-code-indent-offset 2)
-  (setq web-mode-asp-offset 2)
+  (setq web-mode-markup-indent-offset 4)
+  (setq web-mode-css-offset 4)
+  (setq web-mode-script-offset 4)
+  (setq web-mode-php-offset 4)
+  (setq web-mode-java-offset 4)
+  (setq web-mode-code-indent-offset 4)
+  (setq web-mode-asp-offset 4)
   (setq tab-width 2)
   (setq web-mode-engines-alist
-        '(("mako" . "\\.html\\'")
-          ("jinja" . "\\.jinja\\'"))))
+        '(;("mako" . "\\.html\\'")
+          ("jinja" . "\\.jinja\\'")
+          ("blade" . "\\.blade\\."))))
 
 (add-hook 'web-mode-hook 'web-mode-hook)
 
--- layers/+lang/php/config.el
+++ layers/+lang/php/config.el
@@ -1,6 +1,11 @@
 ;; (add-hook 'php-mode-hook 'php-enable-default-coding-style)
 ;; (add-hook 'php-mode-hook 'phpcbf-enable-on-save)
-(add-to-list 'auto-mode-alist '("\\.php" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.php$" . php-mode))
 
 ;; (add-to-list 'load-path (concat temp-directory "php-extras/"))
 
+(setq php-mode-force-pear t)
+(add-hook 'php-mode-hook
+          (lambda ()
+            (setq c-basic-offset 4)
+            (setq tab-with 4)))
