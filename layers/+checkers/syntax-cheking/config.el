1f86976091929c7088d5e23a859dc9dc3816d770
Higashigawa Ryo <higashikawap@unzip.co.jp>
2019-05-29 16:01:20 +0900

    update syntax-checking layer

--- layers/+checkers/syntax-cheking/config.el
+++ layers/+checkers/syntax-cheking/config.el
@@ -1,4 +1,17 @@
+;;; syntax-cheking --- config.el -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;; This file is syntax check layer's config.
+
+;;; Code:
+
+;; flycheck mode on (global)
+(global-flycheck-mode)
+
 (add-hook 'php-mode-hook
           (lambda ()
-            (setq flycheck-phpcs-standard "PSR2")))
-(setq php-mode-coding-style (quote psr2))
+            (defvar flycheck-phpcs-standard "PSR2")))
+(defvar php-mode-coding-style (quote psr2))
+
+;;; config.el ends here
